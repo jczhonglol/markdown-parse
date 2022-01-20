20d8a18fae13bc5844ed212a89161a8acd32cc73
Jacob Zhong <jczhonglol@gmail.com>
2022-01-19 16:25:59 -0800

    added error files and the corresponding fixes
    created three separate files that had no links, had characters in between the links, and had characters after the links. Then coded the fixes to these errors in MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,7 +15,12 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1){
+                break;
+            }
+            if (nextCloseBracket + 1 == openParen) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
