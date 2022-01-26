5bd10cde27cfa9b427be1b7d424b3358c369043c
Jacob Zhong <jczhonglol@gmail.com>
2022-01-26 15:49:17 -0800

    added the MarkdownParseTest.java
    javac -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
    
    java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class MarkdownParseTest {
+    @Test
+    // annotation defines the method below as a JUnit test
+    public void addition (){
+        assertEquals(2, 1+1);
+        // assertEquals checks if the value of the first parameter is
+        // equivalent to the value of the second parameter
+    }
+}
